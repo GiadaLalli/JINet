69bc15af71d05290d1e7abf210b1f30a1feb8219
James Collier <james.collier@vib.be>
2024-02-08 17:00:50 +0100

    Initial attempts to use pyodide to run Python code

---
+++ tests/script.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import plotly.express as px
+
+def scatter_px(df, x, y, target, dpi=300, height=600, width=800, title=None, legend_orientation="v"):
+
+    if x is None:
+        x = df.columns[0]  # First column of the DataFrame
+    if y is None:
+        y = df.columns[1]  # Second column of the DataFrame
+    # Create the scatter plot using Plotly Express
+    fig = px.scatter(df, x=x, y=y, color=target)
+
+    # Set the title if provided
+    if title:
+        fig.update_layout(
+            title=title, 
+            title_x=0.5,  # Center the title
+            title_font=dict(size=20, color="black", family="Arial")
+        )
+
+    # Update the figure layout to adjust DPI and figsize
+    fig.update_layout(
+        autosize=False,
+        width=width,
+        height=height,
+    )
+
+    # Update the legend orientation
+    if legend_orientation == "h":
+        fig.update_layout(
+            legend_title_text='',  # Remove legend title
+            legend=dict(
+                orientation="h",  # Horizontal legend
+                yanchor="top",
+                y=-0.1,
+                xanchor="right",
+                x=1
+            )
+        )
+    elif legend_orientation == "v":
+        fig.update_layout(
+            legend_title_text='')
+
+    # Show the figure
+    fig.show()
+
+# Example usage:
+#url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+#df = pd.read_csv(url, names=['sepal length','sepal width','petal length','petal width','target'])
+#scatter_px(df, "sepal width", "sepal length", "target", dpi=450, height=1000, width=1100, 
+#             title="Scatter Plot on Iris data", legend_orientation="h")
