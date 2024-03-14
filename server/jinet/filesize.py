609b14969e6bfb1af6f3bdf62eaa7a121c291684
James Collier <james.collier@vib.be>
2024-03-14 16:17:14 +0100

    Add an interface for arbitrary file uploads per request from Giada
    * Add database tables for raw files and for access requests
    * Add API and interface for uploading files
    * Extract filesize checking to re-use between packages and sample data
    files
    * Add "ADMIN" link when user is an admin

---
+++ server/jinet/filesize.py
@@ -0,0 +1,29 @@
+"""Define limits on upload file size."""
+
+from io import BytesIO
+from fastapi import Header, UploadFile, HTTPException, status
+
+# 100k
+MAX_CONTENT_LEN = 100_000
+
+
+async def valid_content_len(content_length: int = Header(..., lt=MAX_CONTENT_LEN)):
+    """Limit the Content-Length header to be < 100k."""
+    return content_length
+
+
+def read_upload_file(thefile: UploadFile) -> bytes:
+    """Extract data from an UploadFile but throw if too large."""
+    buf = BytesIO()
+    actual_size = 0
+    for chunk in thefile.file:
+        actual_size += len(chunk)
+        if actual_size > MAX_CONTENT_LEN:
+            raise HTTPException(
+                status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
+                detail="File too large.",
+            )
+
+        buf.write(chunk)
+    buf.seek(0)
+    return buf.read()
--- server/jinet/main.py
+++ server/jinet/main.py
@@ -1,15 +1,21 @@
 """Main entry point."""
 
-from fastapi import FastAPI, APIRouter, Request
+from typing import Optional
+
+from fastapi import FastAPI, APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+
+from sqlmodel import select, Session, asc
 
 from starlette.middleware.sessions import SessionMiddleware
 from Secweb.CrossOriginEmbedderPolicy import CrossOriginEmbedderPolicy
 from Secweb.CrossOriginOpenerPolicy import CrossOriginOpenerPolicy
 
-from jinet import auth, packages
+from jinet import auth, data, packages
 from jinet.config import settings
+from jinet.db import database_session
+from jinet.models import SampleData
 from jinet.templates import templates
 
 app = FastAPI(title="JINet")
@@ -25,13 +31,14 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 api_router = APIRouter()
 api_router.include_router(auth.router)
 api_router.include_router(packages.router, prefix="/packages")
+api_router.include_router(data.router, prefix="/data")
 app.include_router(api_router)
 
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     """Home page."""
-    request.session["from"] = "/"
+    request.session["from"] = "index"
     return templates.TemplateResponse(
         request=request, name="index.html", context=auth.user_in_context(request)
     )
@@ -42,7 +49,7 @@ async def packages(
     request: Request,
 ):
     """List of packages."""
-    request.session["from"] = "/packages"
+    request.session["from"] = "packages"
     return templates.TemplateResponse(
         request=request, name="packages.html", context=auth.user_in_context(request)
     )
@@ -51,7 +58,29 @@ async def packages(
 @app.get("/contribute", response_class=HTMLResponse)
 async def contribute(request: Request):
     """Documentation for createing a package."""
-    request.session["from"] = "/contribute"
+    request.session["from"] = "contribute"
     return templates.TemplateResponse(
         request=request, name="contribute.html", context=auth.user_in_context(request)
+    )
+
+
+@app.get("/data", response_class=HTMLResponse)
+async def data(
+    request: Request,
+    since: Optional[int] = 0,
+    session: Session = Depends(database_session),
+):
+    """Upload example data formats."""
+    request.session["from"] = "data"
+    query = (
+        select(SampleData)
+        .order_by(asc(SampleData.id))
+        .where(SampleData.id >= since)
+        .limit(10)
+    )
+    data = await session.exec(query)
+    return templates.TemplateResponse(
+        request=request,
+        name="data.html",
+        context=auth.user_in_context(request) | {"data": data.all()},
     )
