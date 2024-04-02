74387ced30824feae32a2a18f53d2ee6dd1952ea
James Collier <james.collier@vib.be>
2024-04-02 16:59:27 +0200

    Formatting for the basic scatter plot example

--- doc/examples/scatter.py
+++ doc/examples/scatter.py
@@ -4,7 +4,10 @@ import pandas as pd
 import plotly
 import plotly.express as px
 
-def scatter_px(df, x, y, target, dpi=300, height=600, width=800, title=None, legend_orientation="v"):
+
+def scatter_px(
+    df, x, y, target, dpi=300, height=600, width=800, title=None, legend_orientation="v"
+):
 
     if x is None:
         x = df.columns[0]  # First column of the DataFrame
@@ -16,9 +19,9 @@ def scatter_px(df, x, y, target, dpi=300, height=600, width=800, title=None, leg
     # Set the title if provided
     if title:
         fig.update_layout(
-            title=title, 
+            title=title,
             title_x=0.5,  # Center the title
-            title_font=dict(size=20, color="black", family="Arial")
+            title_font=dict(size=20, color="black", family="Arial"),
         )
 
     # Update the figure layout to adjust DPI and figsize
@@ -31,31 +34,40 @@ def scatter_px(df, x, y, target, dpi=300, height=600, width=800, title=None, leg
     # Update the legend orientation
     if legend_orientation == "h":
         fig.update_layout(
-            legend_title_text='',  # Remove legend title
+            legend_title_text="",  # Remove legend title
             legend=dict(
                 orientation="h",  # Horizontal legend
                 yanchor="top",
                 y=-0.1,
                 xanchor="right",
-                x=1
-            )
+                x=1,
+            ),
         )
     elif legend_orientation == "v":
-        fig.update_layout(
-            legend_title_text='')
+        fig.update_layout(legend_title_text="")
 
     # Show the figure
     return fig
 
+
 # Example usage:
-#url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-#df = pd.read_csv(url, names=['sepal length','sepal width','petal length','petal width','target'])
-#scatter_px(df, "sepal width", "sepal length", "target", dpi=450, height=1000, width=1100, 
+# url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+# df = pd.read_csv(url, names=['sepal length','sepal width','petal length','petal width','target'])
+# scatter_px(df, "sepal width", "sepal length", "target", dpi=450, height=1000, width=1100,
 #             title="Scatter Plot on Iris data", legend_orientation="h")
-    
+
+
 def main(data: Path, x: str, y: str, target: str) -> str:
-    print(f"{x=} {y=}")
     df = pd.read_csv(data)
-    fig = scatter_px(df, x, y, target, dpi=450, height=1000, width=1100, 
-                    title="Scatter Plot on Iris data", legend_orientation="h")
-    return fig.to_html(include_plotlyjs=False, full_html=False, default_height='1000px')
+    fig = scatter_px(
+        df,
+        x,
+        y,
+        target,
+        dpi=450,
+        height=1000,
+        width=1100,
+        title="Scatter Plot on Iris data",
+        legend_orientation="h",
+    )
+    return fig.to_html(include_plotlyjs=False, full_html=False, default_height="1000px")
